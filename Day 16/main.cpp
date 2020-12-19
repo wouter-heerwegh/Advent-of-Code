14e13b8110b684f71ebbff1e58ffc4d9952480d2
Wouter Heerwegh <wouter.heerwegh@gmail.com>
2020-12-19 14:04:51 +0100

    Day 16 part 2

--- Day 16/main.cpp
+++ Day 16/main.cpp
@@ -5,7 +5,9 @@
 #include <map>
 #include <tuple>
 #include <sstream>
-#include <regex>
+#include <algorithm>
+
+bool foundSolution = false;
 
 typedef struct rule{
 	std::string rule;
@@ -45,10 +47,11 @@ std::tuple<std::vector<Rule>, std::vector<Ticket>> readInput(std::string filenam
 			char *buffer = (char *)malloc(200 * sizeof(char));
 			std::pair<int,int> range1;
 			std::pair<int,int> range2;
-			std::sscanf(line.c_str(), "%s %d-%d or %d-%d", buffer, &range1.first, &range1.second, &range2.first, &range2.second);
+			std::sscanf(line.c_str(), "%[^:]: %d-%d or %d-%d", buffer, &range1.first, &range1.second, &range2.first, &range2.second);
 
 			Rule rule;
 			rule.rule = std::string(buffer);
+			//std::cout << buffer << ": " << range1.first << "-" << range1.second << ", " << range2.first << '-' << range2.second << std::endl;
 			rule.data.insert(range1);
 			rule.data.insert(range2);
 
@@ -75,11 +78,11 @@ std::tuple<std::vector<Rule>, std::vector<Ticket>> readInput(std::string filenam
     return std::make_tuple(rules, tickets);
 }
 
-std::tuple<int, std::vector<Ticket>> solvePart1(auto & rules, auto & tickets){
+std::tuple<int, std::vector<Ticket>> solvePart1(std::vector<Rule> & rules, std::vector<Ticket> & tickets){
 	int sum = 0;
 	std::vector<Ticket> goodTickets;
 	for(Ticket &tick: tickets){
-		bool goodTicket = false;
+		bool goodTicket = true;
 		for(int & val: tick.values){
 			bool good = false;
 			for(Rule & rule: rules){
@@ -87,7 +90,6 @@ std::tuple<int, std::vector<Ticket>> solvePart1(auto & rules, auto & tickets){
 					//std::cout << val << ", " << pair.first << "-" << pair.second;
 					if(val >= pair.first && val <= pair.second){
 						good = true;
-						goodTicket = true;
 						//std::cout << ": GOOD";
 					}
 					//std::cout << std::endl;
@@ -95,6 +97,7 @@ std::tuple<int, std::vector<Ticket>> solvePart1(auto & rules, auto & tickets){
 			}
 
 			if(!good){
+				goodTicket = false;
 				sum += val;
 			}
 		}
@@ -107,12 +110,113 @@ std::tuple<int, std::vector<Ticket>> solvePart1(auto & rules, auto & tickets){
 	return std::tuple(sum, goodTickets);
 }
 
+int chooseFields(std::map<int, std::string> & lookup, std::vector<std::pair<std::string, std::map<int, int>>> & possibilities, int tickSize, int ruleSize){
+	if(lookup.size() == ruleSize){
+		foundSolution = true;
+		std::cout << "Solution found!" << std::endl;
+		return 0;
+	}
+
+	for(int i = 0; i < possibilities.size(); i++){
+		for(auto & field: possibilities[i].second){
+			//std::cout <<field.first << ", " << field.second << std::endl;
+			if(field.second >= tickSize && !(lookup.count(field.first) >= 1)){
+				lookup.insert(std::pair<int,std::string>(field.first, possibilities[i].first));
+				//std::cout << "trying: " << field.first << ", " << pos.first << std::endl;
+				auto copy(possibilities);
+				copy.erase(copy.begin() + i);
+				chooseFields(lookup, copy, tickSize, ruleSize);
+				if(foundSolution){
+					return 0;
+				} else {
+					lookup.erase(field.first);
+				}
+			}
+		}
+	}
+
+	//std::cout << "No solution found" << std::endl;
+}
+
+long long solvePart2(std::vector<Rule> & rules, std::vector<Ticket> & tickets){
+	std::map<std::string, std::map<int, int>> possibilities;
+
+	for(Rule & rule: rules){
+		for(Ticket & tick: tickets){
+			for(int i = 0; i < tick.values.size(); i++){
+				bool passed = false;
+				for(auto & pair: rule.data){
+					if(tick.values[i] >= pair.first && tick.values[i] <= pair.second){
+						passed = true;
+					}
+				}
+
+				//std::cout << tick.values[i] << ", " << passed << std::endl;
+				if(passed){
+					//std::cout << "Inserting: " << rule.rule << ": " << i << std::endl;
+					if(possibilities.count(rule.rule) >= 1){
+						if(possibilities[rule.rule].count(i) >= 1){
+							possibilities[rule.rule][i]++;
+						} else {
+							std::map<int, int> newmap = possibilities[rule.rule];
+							newmap.insert(std::pair<int,int>(i, 1));
+							possibilities[rule.rule] = newmap;
+						}
+					} else {
+						std::map<int,int> newmap;
+						newmap.insert(std::pair<int,int>(i, 1));
+						possibilities.insert(std::pair<std::string, std::map<int,int>>(rule.rule, newmap));
+					}
+				}
+			}
+		}
+		
+	}
+
+	std::map<int,std::string> lookup;
+	std::vector<std::pair<std::string, std::map<int,int>>> vec;
+ 
+    std::copy(possibilities.begin(),
+            possibilities.end(),
+            std::back_inserter<std::vector<std::pair<std::string, std::map<int,int>>>>(vec));
+ 
+	for(auto & pair: vec){
+		std::map<int,int> newmap(pair.second);
+		for(auto & pos: pair.second){
+			if(pos.second < tickets.size()){
+				newmap.erase(pos.first);
+			}
+		}
+		pair.second = newmap;
+		std::cout << pair.first << ", " << pair.second.size() << std::endl;
+	}
+
+    std::sort(vec.begin(), vec.end(),
+            [](const std::pair<std::string, std::map<int,int>> & l, const std::pair<std::string, std::map<int,int>>& r) {
+                return l.second.size() < r.second.size();
+            });
+	
+	chooseFields(lookup, vec, tickets.size(), possibilities.size());
+	long long result = 1; 
+	for(auto & val: lookup){
+		if(val.second.find("departure") != std::string::npos){
+			result *= myTicket.values[val.first];
+		}
+		std::cout << val.second << ": " << myTicket.values[val.first] << std::endl;
+	}
+	return result;
+ }
+
 int main(int argc, char const *argv[])
 {
 	auto [rules, tickets] = readInput("input.txt");
 
 	auto [result, tickets2] = solvePart1(rules, tickets);
 
-	std::cout << result << std::endl;
+	std::cout << "Part 1: " << result << std::endl;
+
+	long long result2 = solvePart2(rules, tickets2);
+
+	std::cout << "Part 2: " << result2 << std::endl; 
 	return 0;
 }
